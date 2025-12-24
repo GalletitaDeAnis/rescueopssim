dc7d1b9e24f7224f0699d77ad69be74d8e94aea9
GalletitaDeAnis <paolopizarro685@gmail.com>
2025-12-24 15:56:03 -0400

    Repo hygiene: ignore build outputs and normalize line endings

--- build_test/CMakeFiles/sim_core.dir/src/models/motion.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/sim_core.dir/src/models/motion.cpp.o: \
- /mnt/data/rescueopssim/src/models/motion.cpp /usr/include/stdc-predef.h \
- /mnt/data/rescueopssim/src/models/motion.hpp
--- build_test/CMakeFiles/sim_core.dir/src/planner/kalman.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/sim_core.dir/src/planner/kalman.cpp.o: \
- /mnt/data/rescueopssim/src/planner/kalman.cpp /usr/include/stdc-predef.h \
- /mnt/data/rescueopssim/src/planner/kalman.hpp
